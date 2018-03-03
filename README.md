a2f8ccd3e96d8b89d80b47158805ade1cd15edb5
Mark Coleman <setyourgoalsss@gmail.com>
2018-03-03 13:40:21 -0700

    Adding Cash Register Project and README.md doc

--- README.md
+++ README.md
@@ -3,3 +3,27 @@
 ### Skill Distillery Week 1 Homework
 
 Program to calculate change.
+
+Technologies used:
+
+* Advanced Java Expressions
+* While Loops
+* if Statements
+* Methods
+* Casting Data Types
+
+
+#### Project Description
+
+The objective of this project is to create a cash register that will calculate the exact amount of change returned to a customer based on the purchase price and amount tendered.
+The exact currency returned to the customer will be provided i.e. how many ten dollar bills, five dollar bills, quarters, dimes, etc.
+
+#### How To Run This Project
+
+To run this project, enter the price of the item you'd like to purchase. Next, enter how much money you will be tendering to pay for the item. If funds are insufficient, you will be prompted again to enter the price of the item and the amount of money tendered to pay for the item.
+
+#### Lessons Learned
+
+Casting from a double to an integer was a valuable asset in my program. It helped me to avoid issues with rounding numbers or having too many numbers after the decimal being returned.
+
+Creating the mathematical formula to determine how many $20 bills, $5 bills, etc. was difficult at first. It was important to process the program requirements one step at a time. 
--- src/com/skilldistillery/cashregister/CashRegister.java
+++ src/com/skilldistillery/cashregister/CashRegister.java
@@ -5,16 +5,83 @@ import java.util.Scanner;
 public class CashRegister {
 
 	public static void main(String[] args) {
+		transactionProcess();
+
+	}
+
+	public static void transactionProcess() {
 		Scanner kb = new Scanner(System.in);
-		double twenty = 20.00, ten = 10.00, five = 5.00, one = 1.00, quarter = .25, dime = .10, nickel = .05, 
-		penny = .01, price, tender, returnToCust;
+		double price, tender;
+		int remainder;
+		// creates a boolean set to true for the 'while' loop.
+		boolean flag = true;
+		while (flag) {
+			// Ask the customer what the price of the item they are purchasing is and
+			// storing that into a double so a decimal value can be entered.
+			System.out.print("What is the price of the item you'd like to purchase? ");
+			price = kb.nextDouble();
+			// Ask the customer how much money they will be providing to pay for the item
+			// and store that into a double so a decimal value can be entered.
+			System.out.print("How much money will be tendered to pay for the item? ");
+			tender = kb.nextDouble();
+
+			// create a variable that determines how much change is owed to the customer and
+			// multiply by 100 to convert the doubles into integer values and drop the
+			// decimal values.
+			remainder = (int) ((tender - price) * 100);
+
+			if (tender < price) {
+				System.out.println("You did not provide enough funds. More money is required to pay for this item.");
+				continue;
+			} else if (tender == price) {
+				System.out.println("Thank you for paying with exact change. Have a nice day.");
+				break;
+			} else {
+				int amountOfTwenties = remainder / 2000;
+				remainder %= 2000;
+				if (amountOfTwenties != 0) {
+					System.out.println("$20 bills: " + amountOfTwenties);
+				}
+				int amountOfTens = remainder / 1000;
+				remainder %= 1000;
+				if (amountOfTens != 0) {
+					System.out.println("$10 bills: " + amountOfTens);
+				}
+				int amountOfFives = remainder / 500;
+				remainder %= 500;
+				if (amountOfFives != 0) {
+					System.out.println("$5 bills: " + amountOfFives);
+				}
+				int amountOfOnes = remainder / 100;
+				remainder %= 100;
+				if (amountOfOnes != 0) {
+					System.out.println("$1 bills: " + amountOfOnes);
+				}
+				int amountOfQuarters = remainder / 25;
+				remainder %= 25;
+				if (amountOfQuarters != 0) {
+					System.out.println("Quarters: " + amountOfQuarters);
+				}
+				int amountOfDimes = remainder / 10;
+				remainder %= 10;
+				if (amountOfDimes != 0) {
+					System.out.println("Dimes: " + amountOfDimes);
+				}
+				int amountOfNickels = remainder / 5;
+				remainder %= 5;
+				if (amountOfNickels != 0) {
+					System.out.println("Nickels: " + amountOfNickels);
+				}
+				int amountOfPennies = remainder / 1;
+				remainder %= 1;
+				if (amountOfPennies != 0) {
+					System.out.println("Pennies: " + amountOfPennies);
+				}
+				break;
+			}
+		}
 		
-		System.out.print("What is the price of the item you'd like to purchase? ");
-		price = kb.nextDouble();
-		System.out.print("How much money will be tendered to pay for the item? ");
-		tender = kb.nextDouble();
-		returnToCust = tender - price;
-		System.out.println(returnToCust + " is your change. Thank you.");
+		kb.close();
 	}
 
 }
