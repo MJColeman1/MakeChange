e5fb276de8a72be77618cbb391a423d9efec0216
Mark Coleman <setyourgoalsss@gmail.com>
2018-03-02 17:25:49 -0700

    Adding gitignore

--- src/com/skilldistillery/cashregister/CashRegister.java
+++ src/com/skilldistillery/cashregister/CashRegister.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.cashregister;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		double twenty = 20.00, ten = 10.00, five = 5.00, one = 1.00, quarter = .25, dime = .10, nickel = .05, 
+		penny = .01, price;
+		
+		System.out.println("");
 
 	}
 
